cc3af92ebad56975eeaa12440f4c160c0f473cea
nieshixue <nieshixue@nieshixues-MacBook-Pro.local>
2015-01-03 11:44:25 -0600

    login/createAccount ui

--- client/views/templates/content/login/login.js
+++ client/views/templates/content/login/login.js
@@ -1,5 +1,26 @@
-// placeholder hack for now
-Template.login.rendered = function()
-{
-    Accounts._loginButtonsSession.set('dropdownVisible', true);
-};
+Template.login.events({
+    'submit form': function(event, template){
+        event.preventDefault();
+        var emailVar = template.find('#login-email').value;
+        var passwordVar = template.find('#login-password').value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
+
+Template.register.events({
+	'submit form': function(event, template){
+	    event.preventDefault();
+	    var emailVar = template.find('#register-email').value;
+	    var passwordVar = template.find('#register-password').value;
+	    var passwordVarConfrim = template.find('#register-password-confirm').value;
+
+	    if (passwordVar === passwordVarConfrim) {
+		    Accounts.createUser({
+		        email: emailVar,
+		        password: passwordVar
+		    });
+	    } else {
+	    	return alert("password does not match dumbass.")
+	    }
+	}
+});
--- client/views/templates/header/header.js
+++ client/views/templates/header/header.js
@@ -2,5 +2,9 @@ Template.header.events({
 	'click [name=menuToggle]': function(e) {
 		e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+	},
+
+	'click #back': function(e) {
+		eval(this.back_route);
 	}
 });

f076fcaf7fb04d8aed5b1cb1b8250c04950d7d9a
Nicholas Bel <n.bel@berkeley.edu>
2015-01-04 17:17:35 +0100

    added newDivy Template

---
+++ client/views/templates/content/newDivy/newDivy.js
@@ -0,0 +1,15 @@
+Template.newDivy.events({
+  'submit form': function(e) {
+    e.preventDefault();
+    
+    var divy = {
+      title: $(e.target).find('[name=title]').val(),
+      store: $(e.target).find('[name=store]').val()
+    };
+    
+      divy._id = Divys.insert(divy);
+    // Router.go('divyPage', divy);
+  }
+
+});
+

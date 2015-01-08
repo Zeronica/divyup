9e8d512c010356e5247e4cb7980633e962210129
Zeronica <nieziran1996@gmail.com>
2015-01-07 19:43:16 -0800

    resolve push conflict
    occured when adding functionality

--- client/views/templates/content/checkoutMenu/checkoutMenu.js
+++ client/views/templates/content/checkoutMenu/checkoutMenu.js
@@ -1,12 +1,27 @@
 Template.checkoutMenu.helpers({
-	'OrderItems': function() {
-		return OrderItems.find({order_id: this._id});
-	}
-	
-	totalprice: function() {
-        var total = 0;
-        OrderItems.find({order_id: this._id}).map(function(doc) {
-  	total += doc.price});
-  	return total;
+	'f_itemsInCart': function() {
+		console.log('before render');
+		m = Meteor.myFunctions.getCurrentOrder();
+		return OrderItems.find({order_id: m.order_id});
 	},
+	'f_name': function() {
+		return MenuItems.findOne(this.menu_item_id).name;
+	},
+	'f_price': function() {
+		return MenuItems.findOne(this.menu_item_id).price;
+	},
+	'f_currentOrderTotal': function() {
+		m = CurrentOrders.findOne({user_id: Meteor.userId()});
+		return Meteor.myFunctions.totalInOrder(m.order_id);
+	}
+});
+
+Template.checkoutMenu.events({
+	'click #checkout': function(e) {
+		e.preventDefault();
+		console.log('before m');
+		m = Meteor.myFunctions.getCurrentOrder();
+
+		return Meteor.myFunctions.checkoutCurrentOrder(this._id, m.order_id);
+	}
 });

ca4844255a51349eaf0ed4c1484e794d41eafc5b
Nicholas Bel <n.bel@berkeley.edu>
2015-01-09 00:02:55 +0100

    added myDivys template and modified divyPage template

--- client/views/templates/content/divyPage/divyPage.js
+++ client/views/templates/content/divyPage/divyPage.js
@@ -1,5 +1,27 @@
 Template.divyPage.helpers({
 	comments: function() {
 		return Comments.find({divyId: this._id});
+	},
+
+	'f_departTime': function() {
+		time = DeliveryWindows.findOne({_id: this._id}).depart_time;
+		hour = ~~(time/100);
+		minutes = time % 100;
+		return hour + ":" + minutes;
+	}, 
+
+	'f_arrivalTime': function() {
+		time = DeliveryWindows.findOne({_id: this._id}).arrival_time;
+		hour = ~~(time/100);
+		minutes = time % 100;
+		return hour + ":" + minutes;
+	},
+
+	total: function() {
+	return Divys.findOne({_id: this._id}).total;
+	},
+
+	quota: function() {
+	return Divys.findOne({_id: this._id}).quota;
 	}
 });

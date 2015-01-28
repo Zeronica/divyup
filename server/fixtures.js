15550b3f7a9cbe12842f594c573adaca4fecb75a
Ziran Nie <nieziran1996@gmail.com>
2015-01-27 22:08:42 -0800

    added driver routing to login

--- server/fixtures.js
+++ server/fixtures.js
@@ -99,3 +99,19 @@ if (MenuItems.find().count() === 0) {
 		MenuItems.insert(menuItems[i]);
 	}
 };
+
+// // Divys
+
+// if (Divys.find().count() === 0) {
+// 	var divy_id = Divys.insert({
+// 		store_id = Stores.findOne(),
+// 		archived: false
+// 	});
+// }
+
+// // Orders
+// if (Orders.find().count() === 0) {
+// 	var divy_id = Divys.insert({
+		
+// 	})
+// }

87d69a4406d55945916be865189d067e640ce50d
Ziran Nie <nieziran1996@gmail.com>
2015-01-30 10:36:05 -0800

    resolved issues with duplicate templates in client, temporarily disabled intervalRun

--- client/views/user/templates/content/newDivy/newDivy.js
+++ client/views/user/templates/content/newDivy/newDivy.js
@@ -8,5 +8,11 @@ Template.storeItem.helpers({
 	'f_storename': function() {
 		s = Stores.findOne(this.store_id);
 		return s.storename;
+	},
+
+	'f_deliveryType': function(){
+		if (this.deliver_type === 2)
+			return "Driver delivery";
+		return "Walker delivery";
 	}
 })

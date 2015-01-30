723032fb77df220425b87ca2f8e34d157e80dcd7
Ziran Nie <nieziran1996@gmail.com>
2015-01-29 23:25:11 -0800

    add userHome template

--- client/views/user/templates/content/userHome/userHome.js
+++ client/views/user/templates/content/userHome/userHome.js
@@ -1,2 +1,19 @@
 Template.userHome.helpers({
+	'f_pickupDivys': function(){
+		pickup_id = Meteor.user().profile.pickup_id;
+
+		return PickupDivys.find({pickup_id: pickup_id});
+	}
+});
+
+Template.divyItem.helpers({
+	'f_storename': function() {
+		d = Divys.findOne(this.divy_id);
+		s = Stores.findOne();
+		return s.storename;
+	},
+
+	'f_totalInDivy': function() {
+		return Meteor.myFunctions.totalInDivy(this.divy_id);
+	}
 });
